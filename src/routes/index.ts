9971ed8615969fb6b8cc2b43505f381859d17002
MustafaShesh <mostafagamal9591@yahoo.com>
2022-11-30 22:41:35 +0200

    Input type suggestion

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,7 @@ import imageProcess from '../utilities/imageProcessing';
 const routes = express.Router();
 
 // define a route handler for the image processing
-routes.get('/images', imageProcess, (req, res) => {
+routes.get('/images', imageProcess, (req, res): void => {
   imageProcess;
 });
 
